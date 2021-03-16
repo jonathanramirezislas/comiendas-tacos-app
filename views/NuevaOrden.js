11ca38889fe9563a4e442f6cd0d2517681e6c639
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-15 20:53:09 -0600

    native base

--- views/NuevaOrden.js
+++ views/NuevaOrden.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Container, Button, Text } from 'native-base';
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
 
 const NuevaOrden = () => {
-    return (
-            <Text>Nueva Orden</Text>            
-    )
+
+    const navigation = useNavigation();
+
+    
+    return ( 
+        <Container style={globalStyles.contenedor}>
+            <View style={[globalStyles.contenido, styles.contenido]}>
+                <Button
+                    style={globalStyles.boton}
+                    rounded
+                    block
+                    onPress={ () => navigation.navigate('Menu')  }
+                >
+                    <Text style={globalStyles.botonTexto}>Crear Nueva Orden</Text>
+                </Button>
+            </View>
+        </Container>
+     );
 }
 
-export default NuevaOrden
+const styles = StyleSheet.create({
+    contenido: {
+        flexDirection: 'column',
+        justifyContent: 'center'
+    }
+})
+ 
+export default NuevaOrden;
