d4c2f3fb00a3842867cf0f1c797e0c3aacb68db9
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-15 22:56:09 -0600

    mostrar menu al cliente

--- context/firebase/firebaseState.js
+++ context/firebase/firebaseState.js
@@ -15,11 +15,44 @@ const FirebaseState = props => {
     // useReducer con dispatch  para ejecutar las funciones
     const [ state, dispatch ] = useReducer(FirebaseReducer, initialState);
 
+      // Función que se ejecuta para traer los productos
+      const obtenerProductos = () => {
+  
+
+        // consultar firebase
+        firebase.db
+            .collection('productos')
+            .where('existencia', '==', true) // traer solo los que esten en existencia
+            .onSnapshot(manejarSnapshot); //real-time 
+
+        function manejarSnapshot(snapshot) {
+            let platillos = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id, //id del platillo XD
+                    ...doc.data()//datos del platillo
+                }
+            });
+
+            // Ordenar por categoria con lodash
+            platillos = _.sortBy(platillos, 'categoria');
+
+            // console.log(platillos)
+
+            // Tenemos resultados de la base de datos
+            dispatch({
+                type: OBTENER_PRODUCTOS_EXITO,
+                payload: platillos
+            });
+        }
+    }
+
 
     return (
         <FirebaseContext.Provider
            value={{
-               menu: state.menu
+               menu: state.menu,
+               firebase,
+               obtenerProductos
            }}
         >
             {props.children}
--- views/Menu.js
+++ views/Menu.js
@@ -1,10 +1,84 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext, useEffect, Fragment } from 'react';
+import { StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import {
+    Container,
+    Separator,
+    Content,
+    List,
+    ListItem,
+    Thumbnail,
+    Text,
+    Body
+} from 'native-base';
+import globalStyles from '../styles/global';
+
+import FirebaseContext from '../context/firebase/firebaseContext';
+import PedidoContext from '../context/pedidos/pedidosContext';
+
 
 const Menu = () => {
+
+    // Context de Firebase 
+    const { menu, obtenerProductos } = useContext(FirebaseContext);
+
+    // Context de pedido
+    const { seleccionarPlatillo } = useContext(PedidoContext);
+
+    // Hook para redireccionar
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        obtenerProductos();
+    }, []);
+
+ 
     return (
-            <Text>Menu</Text>            
-    )
+        <Container style={globalStyles.contenedor}>
+            <Content style={{ backgroundColor: '#FFF' }}>
+                <List>
+                    {menu.map((platillo, i) => {
+                        const { imagen, nombre, descripcion, categoria, precio, id } = platillo;
+                        return (
+                            <Fragment key={id}>
+                               
+                                <ListItem
+                                >
+                                    <Thumbnail
+                                        large
+                                        square
+                                        source={{ uri: imagen }}
+                                    />
+                                    <Body>
+                                        <Text>{nombre} </Text>
+                                        <Text
+                                            note
+                                            numberOfLines={2} //props propios de Text
+                                        >
+                                            {descripcion}
+                                        </Text>
+                                        <Text>Precio: $ {precio} </Text>
+                                    </Body>
+                                </ListItem>
+                            </Fragment>
+                        )
+                    })}
+                </List>
+            </Content>
+
+        </Container>
+    );
 }
 
-export default Menu
+const styles = StyleSheet.create({
+    separador: {
+        backgroundColor: '#000',
+    },
+    separadorTexto: {
+        color: '#FFDA00',
+        fontWeight: 'bold',
+        textTransform: 'uppercase'
+    }
+})
+
+export default Menu;
